ef852d3a09f9e1f2777cf4ed5ee41e9e87c356a1
carolaine730 <carolaine.santos@iesb.edu.br>
2026-04-07 20:00:41 -0300

    Finalização da atividade 02

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,34 @@
-import './style.css'
+import "./style.css";
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", (e) => {
+  e.preventDefault ();
+  const matriculaInput = document.querySelector("#Matricula");
+  const matriculaErro = document.querySelector("#MatriculaErro");
+  const senhaInput = document.querySelector("#Senha");
+  const senhaErro = document.querySelector("#SenhaErro");
+
+  matriculaErro.textContent = '';
+  senhaErro.textContent = '';
+
+  if (matriculaInput.value == "") {
+    matriculaErro.textContent = "Matrícula é obrigatória";
+    return;
+  }
+
+  if (parseInt(matriculaInput.value) <= 0) {
+    matriculaErro.textContent = "Matrícula deve ser um número positivo";
+    return;
+  }
+
+  if (senhaInput.value == "") {
+    senhaErro.textContent = "Senha é obrigatória";
+    return;
+  }
+
+  if (senhaInput.value.length < 8) {
+    senhaErro.textContent = "A Senha deve conter pelo menos 8 caracteres";
+    return;
+  }
+});
